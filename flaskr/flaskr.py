df4f0c56d0c68e82ddd0cf64b92833aed4d4acbb
zhaolei <2290938350@qq.com>
2017-10-12 12:29:54 +0800

    update

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -23,6 +23,9 @@ app.config.update(dict(
     UID=''
 ))
 
+# 开启调试
+# app.debug = True
+
 # 设置一个名为FLASKR_SETTINGS的环境变量来设定一个配置文件载入后是否覆盖默认值
 # silent静默开关表示flask不去关心这个键值是否存在。
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -164,4 +167,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
